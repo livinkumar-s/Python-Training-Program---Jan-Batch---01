4986c12af01b25973756e884074d189c5c0a2fbd
LivinkumarStackly <livinkumar@thestackly.com>
2026-01-09 11:41:35 +0530

    Datatypes and Type Casting

---
+++ Datatypes.py
@@ -0,0 +1,43 @@
+a=10
+b=2.22
+c="Hello world"
+d=True
+
+# print(type(a)) #<class 'int'>
+# print(type(b)) #<class 'float'>
+# print(type(c)) #<class 'str'>
+# print(type(d)) #<class 'bool'>
+
+# + ---> Addition
+
+# print(10+6)
+# print(10+6.66)
+
+# print("123"+"231")
+
+# str1="Hello"
+# str2="world"
+
+# print(str1+56)
+
+# print(str1+" "+str2)
+
+# name="Nani"
+# age=42 #"42"
+
+#His name is Nani. He is 42 years old...!
+# print("his name is " + name + ". He is " + str(age) + " years old...!")
+
+# a=10
+# print(type(a))
+# print(type(str(a)))
+
+# print(56)
+
+# a=input("Enter a String: ") #"60"
+
+# print("The prompted input is: "+a)
+
+num=int(input("Enter a Number: "))
+
+print(100+num)
