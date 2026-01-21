531dbd3ce0125b83c5daa0e6fd8a7b2319f62338
LivinkumarStackly <livinkumar@thestackly.com>
2026-01-21 12:21:26 +0530

    Datastructures - List, Tuple

---
+++ Datastructures.py
@@ -0,0 +1,70 @@
+# a="vuhguygyugfygfcyfyfygcfcyhfcfvca"
+
+# print(a[6])
+# print(a[6:]) #012345 6 7 8 9 10
+# print(a[:6]+"guys!") #0123456789 10
+# print(a[3:6][:2][1]+"ve!") 
+# print(a[::-1])
+
+# print(a[-2])
+
+# List 
+
+# print(len(lis1))
+# print(lis1[5:])
+
+# lis1[3]=555
+# lis1.append("Hello")
+# lis1.append("Hi")
+
+# lis1=[2,3,"Two","one",5.55,False] 
+# lis1.extend(["Vijay","Ajith",3,4,5])
+# lis1.extend(["Hello","Hi",1,2,3])
+
+
+# lis1.insert(3,23)
+# lis1.remove(2)
+# lis1.remove(2)
+
+# lis1.pop()
+# lis1.pop()
+# lis1.pop()
+
+# lis1=[2,3,2,4,5,4,6,4,5,67,7,"hello","hi"]
+
+# print(lis1.index(lis1[3]-lis1[2]))
+# print(lis1.index(1))
+# print(lis1.count(1))
+
+# lis1.sort()
+# lis1.reverse()
+# print(lis1)
+# print(lis1[::-1])
+
+# b="Hello"
+# print(b[::-1])
+
+# a=[2,3,4,5,5,4,3,2,0]
+# a.reverse()
+# a[::-1]
+# print(a)
+# if a==a[::-1]:
+#     print("Palindrome")
+
+
+# Tuple 
+
+# tup1=(1,2,3,4,5,4,3,2,1,"hello",[3,4,5])
+# print(tup1[6])
+# tup1[3]=300
+# print(tup1)
+
+person=("Hendry",45)
+
+# name=person[0]
+# age=person[1]
+
+name,age=person
+
+print(name)
+print(age)
--- Loop.py
+++ Loop.py
@@ -65,12 +65,37 @@
 #     print("Hello")
 
 
-secret_number=33
+# secret_number=33
 
-while True:
-    num=int(input("Guess a number: "))
-    if num==secret_number:
-        print("You won!")
-        break
-    else:
-        print("Try again!")
+# while True:
+#     num=int(input("Guess a number: "))
+#     if num==secret_number:
+#         print("You won!")
+#         break
+#     else:
+#         print("Try again!")
+
+# 1 to 100
+
+# div 3 skip
+# div 5 skip
+# div both "py"
+
+# 1,2,4,7,8,11,13,14,"py",16,17,19,22,23,26....100
+
+# dem=8
+
+for i in range(1,9):
+    for j in range(1,9):
+        if i==8:
+            print("* ",end='')
+        elif j==1:
+            print("* ",end='')
+        else:
+            print("  ",end='')
+    print()
+
+
+# print("Hello",end="")
+# print("Hi",end="")
+# print(123)
