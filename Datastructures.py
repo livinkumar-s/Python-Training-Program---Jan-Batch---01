95de6132d9f931ba23af6867e88f8fbffdaa5a73
LivinkumarStackly <livinkumar@thestackly.com>
2026-01-23 11:10:23 +0530

    Datastructures

--- Datastructures.py
+++ Datastructures.py
@@ -59,12 +59,68 @@
 # tup1[3]=300
 # print(tup1)
 
-person=("Hendry",45)
+# person=("Hendry",45)
 
 # name=person[0]
 # age=person[1]
 
-name,age=person
+# name,age=person
 
-print(name)
-print(age)
+# print(name)
+# print(age)
+
+# Set 
+
+set1={
+    1,2,3,4,5,6,7,8,7,6,5,4,3,2,1
+}
+
+set2={
+    4,5,6,7,8,9,10,11,12,13,14
+}
+
+# set1.add(10)
+# set1.remove(6)
+# set1.clear()
+
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+# print(set1.difference(set2))
+# print(set2.difference(set1))
+
+# Dict
+# dict1={
+#     "one":1,
+#     "two":2,
+#     3:"three",
+
+# }
+
+# person={
+#     "name":"Prabhas",
+#     "age":46,
+#     "isMarried":False
+# }
+
+# for i,j in person.items():
+#     print(i)
+#     print(j)
+
+# person["age"]=47
+
+# print(person.keys())
+# print(person.values())
+# print(person.items())
+# person.pop("age")
+
+# print(person)
+
+
+
+
+# list1=[43,23,54,24,54,254,34,2,254,24,2,2,4,3,5]
+# print(list(set(list1)))
+
+
+# list1=[1,2,3,[4,5,6,["seven","eight","nine"]]]
+# print(list1[-1][-1][-1][-1])
