0719239e39e06bffce7efd4b139ac53273ab33fa
LivinkumarStackly <livinkumar@thestackly.com>
2026-01-23 12:18:42 +0530

    Strings

---
+++ String.py
@@ -0,0 +1,53 @@
+# a="Hello world"
+
+# a=a+"...!"
+
+# print(a*3)
+
+# print("hello" in a)
+# print("hello" not in a)
+
+# a=2
+# b=3
+
+# b-=1
+
+# print(a is b)
+
+# a=[1,2,3]
+# b=[1,2,3]
+
+# # print(a==b)
+# print(a is b)
+
+
+# name="Nani"
+# age=42 #"42"
+
+#His name is Nani. He is 42 years old...!
+# print("his name is " + name + ". He is " + str(age) + " years old...!")
+
+
+# print(f"his name is {name}. He is {age} years old...!")
+
+
+# print(name.title())
+# print(name.capitalize())
+
+# print(name.count("from"))
+# print(name.find("you"))
+
+# print(name.isdigit())
+
+# name="Hello guys from india!"
+# print(name.split("l"))
+
+#["He","",""]
+
+# lis=['Hello', 'guys', 'from', 'india!']
+# print("---".join(lis))
+
+a="     nkijddf ljdijvw           "
+# print(a.strip())
+# print(a.lstrip())
+# print(a.rstrip())
