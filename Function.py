a4a4c80d2de20284434f41ca92366c3eb3beae0b
LivinkumarStackly <livinkumar@thestackly.com>
2026-01-23 11:47:17 +0530

    Functions

---
+++ Function.py
@@ -0,0 +1,55 @@
+# def printSteps():
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     print("Step4")
+#     print("Step5")
+
+# printSteps()
+
+# print()
+# input()
+# len()
+# type()
+
+# def dummy(a,b):
+#     print(a+b)
+
+# dummy(34,66)
+# dummy(4,6)
+
+# def printNames(name1,name2):
+#     print("Name 1 is: "+name1)
+#     print("Name 2 is: "+name2)
+
+# printNames(name2="Ken",name1="Karthick")
+
+# def printSum(a,b=10):
+#     print(a+b)
+
+# printSum(20)
+
+# def printMul(*a):
+#     print(sum(a))
+
+# printMul(23,333,22,44,2,4,3,6)
+
+# def sum():
+#     print(1)
+#     print(2)
+#     print(3)
+
+# a=sum() #None
+# print(a)
+
+# def mul(i,j):
+#     return i*j 
+
+# print(mul(3,mul(5,mul(6,mul(7,2)))))
+
+# list1=[1,2,3]
+
+# print(list1.append(4))
+# print(list1.index(1))
+# print(list1.pop())
+# print(list1)
